af2bf19f315e2afcb9a7f0ba5cc42b0346e7dc63
jr-cast <castanos@pm.me>
2022-04-27 11:28:16 -0500

    Add array of object, display elements & style app

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
 import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+const todos = [{
+  description: 'Go to the Gym',
+  completed: false,
+  index: 0
+}, {
+  description: 'Take the dog out',
+  completed: false,
+  index: 1
+}, {
+  description: 'Play videogames',
+  completed: false,
+  index: 2
+}];
 
-  // Lodash, now imported by this js script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('golden');
-  return element;
+const listItems = () => {
+  for (let i = 0; i < todos.length; i++) {
+    const checkBox = document.createElement('input');
+    checkBox.type = 'checkbox';
+    const listItem = document.createElement('li');
+    listItem.innerHTML = todos[i].description;
+    listItem.id = `list${i}`;
+    listItem.prepend(checkBox);
+    const todo = document.getElementById('todolist');
+    todo.append(listItem);
+  }
 }
 
-document.body.appendChild(component());
+listItems();
