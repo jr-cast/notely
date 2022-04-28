c45e0a5fd2cbfc67a9f293e66d85416f22b3a844
jr-cast <castanos@pm.me>
2022-04-28 17:37:12 -0500

    Add updateIndex method, add remove icon

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { remove } from 'lodash';
 import './style.css';
 // import todos from './modules/todosObject.js';
 // import { addNew } from './modules/newTask.js';
@@ -10,6 +11,15 @@ class todosList {
   UpdateList() {
     if (localStorage.todoList) {
       this.List = JSON.parse(localStorage.todoList);
+    }
+  }
+
+  updateIndex() {
+    for (let i = 0; i < this.List.length; i++) {
+      this.List[i].index = i + 1;
+      const stringData = JSON.stringify(this.List);
+      localStorage.setItem('todoList', stringData);
+      this.UpdateList();
     }
   }
 }
@@ -39,10 +49,11 @@ input.addEventListener('keydown', (evt) => {
     todos.List.push(listedItem);
     stringData = JSON.stringify(todos.List);
     localStorage.setItem('todoList', stringData);
+    todos.UpdateList();
   }
 });
 
-// displayt items
+// display items
 const listItems = () => {
   for (let i = 0; i < todos.List.length; i += 1) {
     const checkBox = document.createElement('input');
@@ -50,15 +61,25 @@ const listItems = () => {
     checkBox.id = `check${i}`
     const item = document.createElement('div');
     item.id = `item${i}`;
-    const p = document.createElement('p');
-    p.innerHTML = todos.List[i].description;
+    const task = document.createElement('input');
+    task.className = 'task';
+    task.id = `task${i}`;
+    task.value = todos.List[i].description;
     item.classList.toggle('item');
     item.append(checkBox);
-    item.append(p);
+    item.append(task);
     const str = '<i class="fa-solid fa-ellipsis-vertical"></i>';
     const icon = document.createElement('i');
+    icon.id = `icon${i}`;
     icon.innerHTML = str;
     item.append(icon);
+    let str2 = '<i class="fa-light fa-x"></i>';
+    let trashCan = document.createElement('i');
+    trashCan.className = 'delete';
+    trashCan.id = `trash${i}`;
+    trashCan.innerHTML = str2;
+    trashCan.classList.add('hidden');
+    item.append(trashCan);
     const todo = document.getElementById('todolist');
     todo.append(item);
   }
@@ -66,11 +87,48 @@ const listItems = () => {
 
 listItems();
 
+// add remove icon to item 
+let removeItem = document.getElementsByClassName('item');
+for (let i = 0; i < removeItem.length; i += 1) {
+  removeItem[i].addEventListener('mouseover', () => {
+    document.getElementById(`trash${i}`).classList.remove('hidden');
+    document.getElementById(`icon${i}`).classList.add('hidden');
+  });
+}
+// remove remove icon from item 
+let removedX = document.getElementsByClassName('item');
+for (let i = 0; i < removedX.length; i += 1) {
+  removedX[i].addEventListener('mouseout', () => {
+    let trashCan = document.createElement('i');
+    document.getElementById(`trash${i}`).classList.add('hidden');
+    document.getElementById(`icon${i}`).classList.remove('hidden');
+  });
+};
+
+// add event listener to X button
+let xBtns = document.getElementsByClassName('delete');
+for (let i = 0; i < todos.List.length; i++) {
+  let description = todos.List[i].description;
+  let deletedItem = document.getElementById(`item${i}`);
+  xBtns[i].addEventListener('click', () => {
+    const filtered = todos.List.filter((items) => items.description !== description);
+    const stringData = JSON.stringify(filtered);
+    localStorage.setItem('todoList', stringData);
+    todos.UpdateList();
+    deletedItem.remove();
+    todos.updateIndex();
+  });
+};
+
+
 // remove items from localstorage
-let bool = false;
 for (let i = 0; i < todos.List.length; i += 1) {
   document.getElementById(`check${i}`).addEventListener('change', () => {
-    todos.List[i].completed = (bool = !bool);
+    if (todos.List[i].completed === false) {
+      todos.List[i].completed = true;
+    } else if (todos.List[i].completed === true) {
+      todos.List[i].completed = false;
+    }
   })
 };
 
@@ -84,7 +142,3 @@ document.getElementById('clearA').addEventListener('click', () => {
     window.location.reload();
   };
 });
-
-// const clearBtn = document.getElementById('clearA');
-// clearBtn.addEventListener('click', () => {
-// });
